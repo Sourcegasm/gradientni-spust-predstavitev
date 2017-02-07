6c6284405f074cdd631ea113b5c6fd9b296aee94
Matej Marinko <matejmarinko123@gmail.com>
2017-02-07 19:54:11 +0100

    added base tkinter gui

--- generate_image.py
+++ generate_image.py
@@ -1,15 +1,42 @@
 from matplotlib import pyplot as plt
 import numpy as np
-import PIL
+from PIL import Image, ImageTk
+import tkinter as tk
+import time
 
-x = np.linspace(-10, 10, 500)
-y = np.linspace(-10, 10, 500)
-x, y = np.meshgrid(x, y)
+def get_image(i=0):    
+    x = np.linspace(-10, 10, 500)
+    y = np.linspace(-10, 10, 500)
+    x, y = np.meshgrid(x, y)
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-ax1.contour(x, y, (y**2 - 4*x), [0])
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.contour(x, y, (y**2 - 4*x + i), [0])
 
-canvas = plt.get_current_fig_manager().canvas
-canvas.draw()
-pil_image = PIL.Image.frombytes('RGB', canvas.get_width_height(), canvas.tostring_rgb())
+    canvas = plt.get_current_fig_manager().canvas
+    canvas.draw()
+    plt.close('all')
+
+    pil_image = Image.frombytes('RGB', canvas.get_width_height(), canvas.tostring_rgb())
+    return pil_image
+
+
+
+if __name__ == '__main__':
+    window = tk.Frame()
+    window.master.title("Gradientni spust")
+
+    image = get_image()
+    tk_image = ImageTk.PhotoImage(image)
+    
+    image_label = tk.Label(image=tk_image)
+    image_label.image = tk_image
+    image_label.pack()
+    
+    for i in range(100):
+        tk_image = ImageTk.PhotoImage(get_image(i))
+        image_label.configure(image=tk_image)
+        image_label.image = tk_image
+        window.update()
+
+    window.mainloop()
