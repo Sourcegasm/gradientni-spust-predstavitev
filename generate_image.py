704598da1bf0c0047ff42275eb2f64a047c394ab
Vid Drobnic <vid.drobnic@gmail.com>
2017-02-08 09:51:28 +0100

    plot drawing uses tkinter without pillow

--- generate_image.py
+++ generate_image.py
@@ -1,49 +1,40 @@
-from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 import numpy as np
-from PIL import Image, ImageTk
 import tkinter as tk
 import time
 
 data_set = [[1, 1], [2, 2], [1, 3], [-1, -6]]
 
-def get_image(i=0):
+if __name__ == '__main__':
+    window = tk.Frame()
+    window.master.title("Gradientni spust")
+
     x = np.linspace(-10, 10, 1000)
     y = np.linspace(-10, 10, 1000)
     x, y = np.meshgrid(x, y)
-
-    fig = plt.figure()
+    fig = Figure(figsize=(8, 8), dpi=100)
     ax1 = fig.add_subplot(111)
-    ax1.contour(x, y, (y**2 - 4*x + i), [0])
+    ax1.contour(x, y, (y**2 - 4*x), [0])
 
     points_x = [j[0] for j in data_set]
     points_y = [j[1] for j in data_set]
     ax2 = fig.add_subplot(111)
     ax2.scatter(points_x, points_y)
 
-    canvas = plt.get_current_fig_manager().canvas
-    canvas.draw()
-    plt.close('all')
-
-    pil_image = Image.frombytes('RGB', canvas.get_width_height(), canvas.tostring_rgb())
-    return pil_image
-
-
-
-if __name__ == '__main__':
-    window = tk.Frame()
-    window.master.title("Gradientni spust")
-
-    image = get_image()
-    tk_image = ImageTk.PhotoImage(image)
-
-    image_label = tk.Label(image=tk_image)
-    image_label.image = tk_image
-    image_label.pack()
+    canvas = FigureCanvasTkAgg(fig, master=window.master)
+    canvas.show()
+    canvas.get_tk_widget().pack()
 
     for i in range(100):
-        tk_image = ImageTk.PhotoImage(get_image(i))
-        image_label.configure(image=tk_image)
-        image_label.image = tk_image
+        ax1.contour(x, y, (y**2 - 4*x + i), [0])
+        ax2.scatter(points_x, points_y)
+        canvas.draw()
+        ax1.clear()
+
         window.update()
 
     window.mainloop()
