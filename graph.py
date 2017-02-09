5105110f90e796dec0d99f39383bb889e7039d35
Matej Marinko <matejmarinko123@gmail.com>
2017-02-09 19:11:04 +0100

    minor code changes

--- graph.py
+++ graph.py
@@ -7,54 +7,59 @@ import tkinter as tk
 
 from gradient_decent import calculate_gradient, get_data
 
-done = False
-
-while not done:
-    try:
-        print('Select input data set:')
-        print(' 1    Mars half year')
-        print(' 2    Mars full (whole year measured every Earth month)')
-        print(' 3    Mars small (every fourth point of \'Mars full\')')
-        print(' 4    Earth full (every 14 days)')
-        print(' 5    Saturn full (every 100 days since 1987 = one Saturn year)')
-        print(' 6    custom file path')
-
-        answer = int(input('Your selection: '))
-        
-        if answer == 1: data = get_data('data/mars_half_year.csv')
-        elif answer == 2: data = get_data('data/mars_full.csv')
-        elif answer == 3: data = get_data('data/mars_full.csv')[::4]
-        elif answer == 4: data = get_data('data/earth.csv')
-        elif answer == 5: data = get_data('data/saturn.csv')
-        elif answer == 6: data = get_data(input('Path: '))
-        else: continue
-
-        print()
-        print('Select start parameters:')
-        print(' 1   default [10, 0, 10, 0, 0, 0]')
-        print(' 2   Mars approximation [-100, 0, -100, -300, 200, 30000]')
-        print(' 3   custom params')
-
-        answer = int(input('Your selection: '))
-        if answer == 1: params = [10, 0, 10, 0, 0, -300]
-        elif answer == 2: params = [-100, 0, -100, -300, 200, 30000]
-        elif answer == 3: params = [int(i) for i in input('Params separated by ,: ').split(',')]
-        else: continue
-
-        print()
+def select_data():
+    done = False
+    while not done:
         try:
-            step = float(input('Define step (default is 1e-6): '))
-        except:
-            step = 1e-6
-        done = True
-    except ValueError:
-        print('Invalid input!')
-        print()
+            print('Select input data set:')
+            print(' 1    Mars half year')
+            print(' 2    Mars full (whole year measured every Earth month)')
+            print(' 3    Mars small (every fourth point of \'Mars full\')')
+            print(' 4    Earth full (every 14 days)')
+            print(' 5    Saturn full (every 100 days since 1987 = one Saturn year)')
+            print(' 6    custom file path')
 
-print()
-print('Procesing...')
+            answer = int(input('Your selection: '))
+            
+            if answer == 1: data = get_data('data/mars_half_year.csv')
+            elif answer == 2: data = get_data('data/mars_full.csv')
+            elif answer == 3: data = get_data('data/mars_full.csv')[::4]
+            elif answer == 4: data = get_data('data/earth.csv')
+            elif answer == 5: data = get_data('data/saturn.csv')
+            elif answer == 6: data = get_data(input('Path: '))
+            else: continue
+
+            params_list = [[10, 0, 10, 0, 0, -300], [-100, 0, -100, -300, 200, 30000]]
+
+            print('\nSelect start parameters:')
+            print(' 1   default', params_list[0])
+            print(' 2   Mars approximation', params_list[1])
+            print(' 3   custom params')
+
+            try:
+                answer = int(input('Your selection: '))
+            except:
+                answer = 1
+            if answer == 1: params = params_list[0]
+            elif answer == 2: params = params_list[1]
+            elif answer == 3: params = [float(i) for i in input('Params separated by ,: ').split(',')]
+            else: continue
+
+            print()
+            try:
+                step = float(input('Define step (default is 1e-6): '))
+            except:
+                step = 1e-6
+            done = True
+        except ValueError:
+            print('Invalid input!\n')
+            
+    return data, params, step
+
 
 if __name__ == '__main__':
+    data, params, step = select_data()
+    print('Procesing...')
     window = tk.Frame()
     window.master.title("Gradientni spust")
 
